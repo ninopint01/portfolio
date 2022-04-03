ee1a2b7fa1fd74bc3eb42a6d5ea2d828fdc8e2b2
ninopint01 <100156122+ninopint01@users.noreply.github.com>
2022-04-03 18:09:35 +0200

    Animation of Logo on home right side

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
 import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
 import './index.scss';
 
 const Home = () => {
@@ -36,8 +37,11 @@ const Home = () => {
                 />
                 </h1>
                 <h2>Frontend Developer / Javascript Expert / Youtuber</h2>
-                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+                <Link to="/contact" className='flat-button'>
+                    CONTACT ME
+                </Link>  
             </div>
+            <Logo />
         </div>
     )
 }
