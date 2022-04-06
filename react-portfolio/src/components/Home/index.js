34c68138d3c237fec22bb4fc103f1a6683692bef
ninopint01 <100156122+ninopint01@users.noreply.github.com>
2022-04-06 22:32:43 +0200

    Pacman page loader

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -4,6 +4,7 @@ import LogoTitle from '../../assets/images/logo-s.png';
 import AnimatedLetters from '../AnimatedLetters';
 import Logo from './Logo';
 import './index.scss';
+import Loader from 'react-loaders';
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -17,32 +18,35 @@ const Home = () => {
     }, [])
 
     return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                <span className={letterClass}>H</span>
-                <span className={`${letterClass} _12`}>i,</span>    
-                <br/>
-                <span className={`${letterClass} _13`}>I</span>
-                <span className={`${letterClass} _14`}>'m</span>
-                <img src={LogoTitle} alt="developer" />
-                <AnimatedLetters letterClass={letterClass}
-                strArray={nameArray}
-                index={15}
-                />
-                <br/>
-                <AnimatedLetters letterClass={letterClass}
-                strArray={jobArray}
-                index={22}
-                />
-                </h1>
-                <h2>Frontend Developer / Javascript Expert / Youtuber</h2>
-                <Link to="/contact" className='flat-button'>
-                    CONTACT ME
-                </Link>  
+        <>
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>    
+                    <br/>
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span>
+                    <img src={LogoTitle} alt="developer" />
+                    <AnimatedLetters letterClass={letterClass}
+                    strArray={nameArray}
+                    index={15}
+                    />
+                    <br/>
+                    <AnimatedLetters letterClass={letterClass}
+                    strArray={jobArray}
+                    index={22}
+                    />
+                    </h1>
+                    <h2>Frontend Developer / Javascript Expert / Youtuber</h2>
+                    <Link to="/contact" className='flat-button'>
+                        CONTACT ME
+                    </Link>  
+                </div>
+                <Logo />
             </div>
-            <Logo />
-        </div>
+            <Loader type='pacman' />
+        </>
     )
 }
 
