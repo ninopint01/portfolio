81854d72ded7f1395359ff22d5f9500a4cf27d2e
ninopint01 <100156122+ninopint01@users.noreply.github.com>
2022-04-09 12:44:56 +0200

    Contact page

---
+++ react-portfolio/src/components/Contact/index.js
@@ -0,0 +1,98 @@
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState, useRef } from 'react'
+import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+
+const Contact = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const refForm = useRef()
+    
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 3000)
+    }, [])
+
+    const sendEmail = (event) => {
+        event.preventDefault()
+
+        emailjs
+            .sendForm(
+                'gmail',
+                'templateID',
+                refForm.current,
+                'userToken'
+            )
+            .then(
+                () => {
+                    alert('Message successfully sent')
+                    window.location.reload(false)
+                },
+                () => {
+                    alert('Failed to send the message, please try again')
+                }
+            )
+    }
+
+    return (
+        <>
+            <div className="container contact-page">
+                <div className="text-zone">
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['C','o','t','a','c','t',' ','m','e']}
+                            index={15}
+                        />
+                    </h1>
+                    <p>
+                        I am interested in freelance opportunities - especially ambitious or
+                        large projects. However, if you have other request or question,
+                        don't hesitate to contact me using below form either.
+                    </p>
+                    <div className='contact-form'>
+                        <form ref={refForm} onSubmit={sendEmail}>
+                            <ul>
+                                <li className='half'>
+                                    <input type='text' name='name' placeholder='Name' required />
+                                </li>
+                                <li className='half'>
+                                    <input type='email' name='email' placeholder='Email' required />
+                                </li>
+                                <li>
+                                    <input placeholder='Subject' type='text' name='subject' required />
+                                </li>
+                                <li>
+                                    <textarea placeholder='Message' name='message' required></textarea>
+                                </li>
+                                <li>
+                                    <input type='submit' className='flat-button' value='SEND' />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+                <div className='info-map'>
+                    Slobodan,
+                    <br/>
+                    Country,
+                    <br/>
+                    Street address 12, 10000 <br/>
+                    City big <br/>
+                    <span>freelancerslobodan@gmail.com</span>
+                </div>
+                <div className='map-wrap'>
+                    {/* <MapContainer center={[44.96369, 19.61069]} zoom={13}>
+                        <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+                        <Marker position={[44.96369, 19.61069]}>
+                            <Popup>Come here to play if you dare :)</Popup>
+                        </Marker>
+                    </MapContainer> */}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Contact
